1dd67d36f3e88aa1740ea39e2b68c3eaa11914a2
NataljaB <natalja.buljko@gmail.com>
2020-08-30 19:31:39 +0300

    code connected (outlet) and night mode image

--- ConverterTemperature/ViewController.swift
+++ ConverterTemperature/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var tempSlider: UISlider!
+    
+    
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fahrenheitLabel.text = "32º F"
+        
+        
     }
 
 
