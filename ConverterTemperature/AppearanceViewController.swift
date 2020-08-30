af7ccedffa01499d46d4d7ea2f27d399a5e14185
NataljaB <natalja.buljko@gmail.com>
2020-08-31 00:04:41 +0300

    two view controllers added

---
+++ ConverterTemperature/AppearanceViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AppearanceViewController.swift
+//  ConverterTemperature
+//
+//  Created by natalja.buljko on 30/08/2020.
+//  Copyright © 2020 natalja.buljko. All rights reserved.
+//
+
+import UIKit
+
+class AppearanceViewController: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func openSettingsTapped(_ sender: Any) {
+    }
+    
+    
+
+}
